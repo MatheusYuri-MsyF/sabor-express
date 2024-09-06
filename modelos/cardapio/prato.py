2492626fd94253f63480ee197a1ce7d5313f8f61
Matheus Yuri Miguel <138153599+MatheusYuri-MsyF@users.noreply.github.com>
2024-09-05 23:35:10 -0300

    cardapio/prato.py documentado

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,12 +1,29 @@
 from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Prato(ItemCardapio):
+    """Representa um prato no cardápio, herdando de ItemCardapio.
+    Args:
+        nome (str): Nome do prato.
+        preco (float): Preço do prato.
+        descricao (str): Descrição detalhada do prato.
+    """
     def __init__(self, nome, preco, descricao):
-        super().__init__(nome, preco)
+        """Inicializa uma nova instância de Prato.
+        Args:
+            nome (str): Nome do prato.
+            preco (float): Preço do prato.
+            descricao (str): Descrição detalhada do prato.
+        """
+        super().__init__(nome, preco) # Chama o construtor da classe pai
         self.descricao = descricao
 
     def __str__(self):
+        """Retorna uma representação em string do prato, utilizando apenas o nome.
+        Returns:
+            str: Nome do prato.
+        """
         return self._nome
 
     def aplicar_desconto(self):
+        """Aplica um desconto de 5% no preço do prato."""
         self._preco -= (self._preco * 0.05)
