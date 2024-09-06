5bb302ed9aaf0d0d1b64d92f90f3af4329f77317
Matheus Yuri Miguel <138153599+MatheusYuri-MsyF@users.noreply.github.com>
2024-09-05 23:31:48 -0300

    cardapio/bebida.py documentado

--- modelos/cardapio/bebida.py
+++ modelos/cardapio/bebida.py
@@ -2,13 +2,32 @@ from modelos.cardapio.item_cardapio import ItemCardapio
 
 
 class Bebida(ItemCardapio):
+    """Representa um item de bebida no cardápio, herdando de ItemCardapio.
+    Args:
+        nome (str): Nome da bebida.
+        preco (float): Preço da bebida.
+        sabor (str): Sabor da bebida.
+        tamanho (str): Tamanho da bebida (pequeno, médio, grande, etc).
+    """
     def __init__(self, nome, preco, sabor, tamanho):
-        super().__init__(nome,preco)
+        """Inicializa uma nova instância de Bebida.
+        Args:
+            nome (str): Nome da bebida.
+            preco (float): Preço da bebida.
+            sabor (str): Sabor da bebida.
+            tamanho (str): Tamanho da bebida.
+        """
+        super().__init__(nome,preco) # Chama o construtor da classe pai
         self._sabor = sabor
         self.tamanho = tamanho
 
     def __str__(self):
+        """Retorna uma representação em string da bebida, utilizando apenas o nome.
+        Returns:
+            str: Nome da bebida.
+        """
         return self._nome
     
     def aplicar_desconto(self):
+        """Aplica um desconto de 8% no preço da bebida."""
         self._preco -= (self._preco * 0.08)
