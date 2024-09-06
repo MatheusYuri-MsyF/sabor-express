02184f712e4b5cd3c89f43f8ca40c246ee764f91
Matheus Yuri Miguel <138153599+MatheusYuri-MsyF@users.noreply.github.com>
2024-09-05 23:50:16 -0300

    Sabor-Express-venv/main.py documentado

--- Sabor-Express-venv/main.py
+++ Sabor-Express-venv/main.py
@@ -6,16 +6,29 @@ app = FastAPI()
 
 @app.get('/api/restaurantes/')
 def get_restaurantes(restaurante: str = Query(None)):
+    """
+    Endpoint para buscar informações sobre restaurantes.
+
+    Este endpoint permite buscar todos os restaurantes ou um restaurante específico.
+
+    Args:
+        restaurante (str, optional): Nome do restaurante a ser buscado. Defaults to None.
+
+    Returns:
+        dict: Um dicionário contendo os dados dos restaurantes, ou uma mensagem de erro.
+    """
     url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
     response = requests.get(url)
     '''
     Endpoint para exibir cardapios!
     '''
     if response.status_code == 200:
+        # A requisição à API foi bem-sucedida
         dados_json = response.json()
         if restaurante is None:
+            # Retorna todos os dados dos restaurantes
             return {'Dados':dados_json}
-        
+        # Filtra os dados para encontrar o restaurante específico
         dados_restaurante = []
         for item in dados_json:
             if item['Company'] == restaurante:
@@ -25,4 +38,5 @@ def get_restaurantes(restaurante: str = Query(None)):
                 "description": item['description']})
         return {'Restaurante':restaurante, 'Cardapio':dados_restaurante}    
     else:
+        # Houve um erro na requisição à API
         return {'Erro':f'{response.status_code} - {response.text}'}
